34e833232dc9a05ebf625a48e306f8b480091f59
esthefanialondono62 <esthefanialondono62@gmail.com>
2026-01-26 19:25:56 -0800

    module2.txt

--- Module 2/module2.cpp
+++ Module 2/module2.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+cout << " Hello Inventory System! " << '\n';
+
+cout << " Data Types: " << '\n';
+
+cout << " 1. Size of int: " << sizeof(int) << " Bytes " << '\n';
+cout << " 2. Size of float: " << sizeof(float) << " Bytes " << '\n';
+cout << " 3. Size of char: " << sizeof(char) << " Bytes " << '\n';
+
+
+
+
+
+return 0;    
+}
