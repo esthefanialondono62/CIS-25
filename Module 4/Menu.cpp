59f2a1e3aba5d50574627c069fd49b0dc61954b6
Esthefania <esthefanialondono62@gmail.com>
2026-02-08 11:36:57 -0800

    Update Menu.cpp

--- Module 4/Menu.cpp
+++ Module 4/Menu.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  double option = 0;
+   
+    cout << " Welcome to Esthefania's Menu! " << '\n';
+
+    cout << " Enter the number of food: " << endl;
+    cin >> option;
+
+    if (option == 1){
+
+        cout << " Breakfast: Baguel, Eggs and Coffee " << endl;
+
+    }else if (option == 2){
+
+        cout << " Lunch: Sushi Rolls " << endl;
+
+    }else if (option == 3){
+
+        cout << " Dinner: Mac and cheese " << endl;
+
+    }else{
+
+        cout << " The number does not exist in my menu! "  << endl;
+    
+    }
+    
+    return 0;        
+}
